cc13aa3c18ef7b87942d79f8c3279053b2e311e9
jarosek88 <pernecky2@gmail.com>
2026-02-24 19:59:52 +0100

    update

--- game/admin.py
+++ game/admin.py
@@ -3,12 +3,9 @@ from .models import Postava, Predmet, Nepriatel
 
 @admin.register(Postava)
 class PostavaAdmin(admin.ModelAdmin):
-    # Tu definuješ, ktoré stĺpce chceš vidieť v zozname
     list_display = ('meno', 'pouzivatel', 'level', 'zlato') 
-    # Pridá filter na pravú stranu, aby si mohol filtrovať podľa hráčov
     list_filter = ('pouzivatel',)
 
-admin.site.register(Postava)
 admin.site.register(Predmet)
 admin.site.register(Nepriatel)
 
