5d7b2d9e137fffb42af914b682a2f1b5d8c27ef9
jarosek88 <pernecky2@gmail.com>
2026-03-30 17:33:33 +0200

    Aktualizácia: Oprava vizuálu a hry.

--- game/views.py
+++ game/views.py
@@ -2,12 +2,23 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.shortcuts import get_object_or_404, redirect , render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Postava, Predmet, Nepriatel
 from .forms import PostavaForm
 import random
 
-@login_required # Postavy vidí len prihlásený uživateľ
+# --- REGISTRÁCIA A ZOZNAM ---
+def registracia(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('zoznam_postav')
+    else: form = UserCreationForm()
+    return render(request, 'registration/registracia.html', {'form': form})
+
+@login_required
 def zoznam_postav(request):
     postavy = Postava.objects.filter(pouzivatel=request.user)
     return render(request, 'game/zoznam.html', {'postavy': postavy})
@@ -17,282 +28,98 @@ def nova_postava(request):
     if request.method == 'POST':
         form = PostavaForm(request.POST)
         if form.is_valid():
-            postava = form.save(commit=False) # Ešte neukladaj do DB
-            postava.pouzivatel = request.user # Priradíme aktualné prihlaseného človeka
-            postava.save() # Teraz uložíme aj s majiteľom
-            return redirect('/postavy/') # Po úspechu vráti na zoznam
-    else:
-        form = PostavaForm()
-
+            postava = form.save(commit=False)
+            postava.pouzivatel = request.user
+            postava.save()
+            return redirect('zoznam_postav')
+    else: form = PostavaForm()
     return render(request, 'game/formular.html', {'form': form})
 
-def trenovat_postavu(request, postava_id):
-    from django.shortcuts import get_object_or_404
-    postava = get_object_or_404(Postava, id=postava_id)
+# --- KRČMA ---
+@login_required
+def krcma(request, postava_id):
+    postava = get_object_or_404(Postava, id=postava_id, pouzivatel=request.user)
+    hlasky = ["Daj si pivo!", "V lese sú vlky.", "Tvoj meč je tupý."]
+    return render(request, 'game/krcma.html', {'postava': postava, 'hlaska': random.choice(hlasky)})
 
-    # Logika náhodnu silu
-    postava.sila += random.randint(3, 8)
-
-    # Level up systém, ak má silu viac ako 50 pôjde o level hore
-    if postava.sila >= 50 and postava.level == 1:
-        postava.level = 2
-        postava.max_hp += 20 # Bonusové HP za level
-        postava.hp = postava.max_hp
-    
-    postava.hp += 15 # Prida za každý trening život
-
-    # poistka na presiahnutie maxima
-    if postava.hp > postava.max_hp:
-        postava.hp = postava.max_hp
-
-    # Výpočet levelu za každých 100 jeden level
-    novy_level = (postava.sila // 100) + 1
-
-    if novy_level > postava.level:
-        postava.level = novy_level
-        # Bonus pri každom level pridame Hp o 20
-        postava.max_hp += 20
-        postava.hp = postava.max_hp # Vliečime ho trochu
-
-    postava.save() # Zapíše sa zmena v databáze
-    return redirect('/postavy/') #vráti sa naspat na zoznam
-
-def dobrodruzstvo(request, postava_id):
-    postava = get_object_or_404(Postava, id=postava_id)
-    lokalita = request.GET.get('lokalita', 'les') # Zistíme kam hráč klikol
-
-    if lokalita == 'odmena':
-        #Skontrolujeme či ma nárok na odmenu
-        if postava.sila >= 30 and not postava.quest_sila_splneny:
-            postava.zlato += 100
-            postava.quest_sila_splneny = True
-            postava.save()
-            messages.success(request, f"🎁 Výborne! Za tvoju silu získavaš odmenu 100 zlata.")
-        elif postava.quest_sila_splneny:
-            messages.info(request, f"Túto odmenu si si už vybral")
-        else:    
-            messages.error(request, f"Ešte nie si dosť silný na túto odmenu!")
-        return redirect('/postavy/')
-    
-    if lokalita == 'odmena_level':
-        if postava.level >= 5 and not postava.quest_level_splneny:
-            postava.zlato += 200
-            postava.quest_level_splneny = True
-            postava.save()
-            messages.success(request, "Paráda! Za 5. level získavaš 200 zlata!")
-        return redirect('/postavy/') # Vždy sa vráť späť, aby ťa nezožral troll!
-    
-    is_dungeon = (lokalita == 'dungeon')
-
-    if lokalita == 'les':
-        # Vyberieme len slabších sila pod 50
-        vsetci_nepriatelia = Nepriatel.objects.filter(sila__lt=50)
-    elif is_dungeon:
-        vsetci_nepriatelia = Nepriatel.objects.filter(sila__gte=50, sila__lt=100)
-    elif lokalita == 'boss':
-        if postava.level < 8:
-            messages.error(request, f"{postava.meno} je príliš slabý na súboj s Bossom! Potrebuješ aspoň level 8.")
-            return redirect('/postavy/')
-        
-        if postava.level < 10:
-            vsetci_nepriatelia = Nepriatel.objects.filter(sila__gte=100, sila__lt=151)
-        elif postava.level < 13:
-            vsetci_nepriatelia = Nepriatel.objects.filter(sila__gte=151, sila__lte=251)
-        else:
-            vsetci_nepriatelia = Nepriatel.objects.filter(sila__gte=251)
-    else:
-        vsetci_nepriatelia = Nepriatel.objects.all()
-
-    # Vyberieme nepriateľa z vyfiltrovaneho zoznamu
-    if  vsetci_nepriatelia.exists():
-        nepriatel = random.choice(vsetci_nepriatelia)
-    else:
-        #Ak by bol les prazdny
-        nepriatel = random.choice(Nepriatel.objects.all())
-
-    # Logika súboja
-    # Nepriateľ udrie svojou silou, ale obrana stlmí úder
-    poskodenie = nepriatel.sila - postava.obrana
-    if poskodenie < 0: poskodenie = 0 #Obrana je silnejšia ako útok
-
-    postava.hp -= poskodenie
-    # Pridáme správu o súboji
-    messages.warning(request, f"Stretol si, {nepriatel.ikona} {nepriatel.nazov}! Utrpel si {poskodenie} poškodenia.")
-
-    # Ak hrdina prežil dostane XP
-    if postava.hp > 0:
-        vyhra = True
-    else:
-        vyhra = False
-
-    if postava.hp <= 0:
-        messages.error(request, f"Bohužiaľ, {nepriatel.nazov} ťa porazil. {postava.meno} padol v boji...")
-        #Hardcore postava zomrela, vymažeme ju
-        postava.delete()
-        return redirect('/postavy/') # Navrát na zoznam, postava už tam nebude
-
-    # šanca na najdenie predmetu
-    sanca = random.randint(1, 10)
-    if sanca <= 3 or lokalita == 'boss':
-        # 1. Kontrola limitu batohu (kapacita 5 slotov)
-        if postava.batoh.count() >= 5:
-            messages.warning(request, "Tvoj batoh je plný! Nič nové si neuniesol.")
-        else:
-            typ_lootu = random.choice(['lektvar', 'zbran', 'brnenie'])
-            
-            # 2. Určenie vzácnosti (rarity)
-        
-            vzacnost_sanca = random.randint(1, 100)
-            if lokalita == 'boss': # Boss ma 100% sancu na artefakt
-                vzacnost = 'legendary'
-                nasobitel = 5
-                prefix = "Epický artefakt"
-
-            elif lokalita == 'dungeon':
-                # Špeciálne šance pre Dungeon (30% na Legendárny)
-                if vzacnost_sanca <= 30:
-                    vzacnost = 'legendary'
-                    nasobitel = 4
-                    prefix = "Dungeonový Artefakt "
-                else:
-                    vzacnost = 'rare'
-                    nasobitel = 2
-                    prefix = "Vzácny "
-            else:
-                # Pôvodná logika pre Les (10% na Legendárny)
-                if vzacnost_sanca <= 10:
-                    vzacnost = 'legendary'
-                    nasobitel = 3
-                    prefix = "Legendárny "
-                elif vzacnost_sanca <= 30:
-                    vzacnost = 'rare'
-                    nasobitel = 2
-                    prefix = "Vzácny "
-                else:
-                    vzacnost = 'common'
-                    nasobitel = 1
-                    prefix = ""
-
-            # 3. Vytvorenie predmetu s priradením vzácnosti a majiteľa
-            if typ_lootu == 'lektvar':
-                novy_predmet = Predmet.objects.create(
-                    majitel=postava, 
-                    nazov=f"{prefix} Magický lektvar", 
-                    typ='lektvar', 
-                    bonus_hp=30 * nasobitel,
-                    rarity=vzacnost
-                )
-            elif typ_lootu == 'zbran':
-                novy_predmet = Predmet.objects.create(
-                    majitel=postava, 
-                    nazov=f"{prefix} Ostrý meč", 
-                    typ='zbran', 
-                    bonus_sila=random.randint(5, 20) * nasobitel,
-                    rarity=vzacnost
-                )
-            elif typ_lootu == 'brnenie':
-                novy_predmet = Predmet.objects.create(
-                    majitel=postava, 
-                    nazov=f"{prefix} Kožená vesta", 
-                    typ='brnenie', 
-                    bonus_obrana=random.randint(3, 12) * nasobitel,
-                    rarity=vzacnost
-                )
-            
-            messages.info(request, f"V tráve si našiel: {novy_predmet.nazov}")
-
-
-    if vyhra:
-        postava.xp += nepriatel.xp_odmena
-
-        # Logika pre level up
-        if postava.xp >= postava.xp_na_level:
-            postava.level += 1
-            postava.xp -= postava.xp_na_level # Zvyšok XP ostáva do dalšieho levelu
-            postava.xp_na_level = int(postava.xp_na_level * 1.5) # Každý daľší level je tažší
-            postava.max_hp += 20
-            postava.hp = postava.max_hp # Pri level up sa uplne vylieči
-            messages.success(request, f"💥LEVEL UP! Teraz si level {postava.level}!")    
-
+# Pôvodný odpočinok (zadarmo / plné zdravie)
+@login_required
+def odpocinok(request, postava_id):
+    postava = get_object_or_404(Postava, id=postava_id, pouzivatel=request.user)
+    postava.hp = postava.max_hp
     postava.save()
-    return redirect('/postavy/')
+    messages.success(request, "Si plný síl!")
+    return redirect('krcma', postava_id=postava.id)
 
-def pouzit_predmet(request, predmet_id):
-    predmet = get_object_or_404(Predmet, id=predmet_id)
-    postava = predmet.majitel
-
-    if predmet.typ == 'lektvar':
-        postava.hp += predmet.bonus_hp
-        if postava.hp > postava.max_hp:
-            postava.hp = postava.max_hp
-        predmet.delete() # Lektvár sa vymaže s batohu
-
-    elif predmet.typ == 'zbran':
-        postava.sila += predmet.bonus_sila #pridame silu natrvalo
-        predmet.delete() # meč sme nasadili a staa sa súčasťou sily
-
-    elif predmet.typ == 'brnenie':
-        postava.obrana += predmet.bonus_obrana # pridáme obranu natrvalo
-        predmet.delete()
-
-    postava.save() # Uložíme hrdinovi
-    return redirect('/postavy/')
-
-def predat_predmet(request, predmet_id):
-    predmet = get_object_or_404(Predmet, id= predmet_id)
-    postava = predmet.majitel
-
-    postava.zlato += 50 # Pevna cena za predaj
-    postava.save()
-
-    nazov_predmetu = predmet.nazov
-    predmet.delete()
-
-    messages.success(request, f"Predal si {nazov_predmetu} za 50 zlatých!")
-    return redirect('/postavy/')
-
-def kupit_lektvar(request, postava_id):
-    postava = get_object_or_404(Postava, id = postava_id)
-    cena = 50
-
+# Nový odpočinok (pivo / jedlo)
+@login_required
+def oddychnut(request, postava_id, typ_odpocinok):
+    postava = get_object_or_404(Postava, id=postava_id, pouzivatel=request.user)
+    cena, liecenie = (10, 20) if typ_odpocinok == 'pivo' else (30, 50)
     if postava.zlato >= cena:
         postava.zlato -= cena
-        postava.hp += 30
-        if postava.hp > postava.max_hp:
-            postava.hp = postava.max_hp
-        
+        postava.hp = min(postava.hp + liecenie, postava.max_hp)
         postava.save()
-        messages.success(request, f"Kúpil si si lektvar! HP doplnené na {postava.hp}.")
-    else:
-        messages.success(request, f"Nemáš dosť zlata na lektvar!") 
+        messages.success(request, f"Vyliečený o {liecenie} HP!")
+    else: messages.error(request, "Málo zlata!")
+    return redirect('krcma', postava_id=postava.id)
 
-    return redirect('/postavy/')
-
-def nakup(request, postava_id, typ_vylepsenia):
-    postava = get_object_or_404(Postava, id=postava_id)
-    cena = 100 # Jednotna cena za tréning
-
-    if postava.zlato >= cena:
-        postava.zlato -= cena
-        if typ_vylepsenia == 'sila':
-            postava.sila += 10
-            messages.success(request, f"Poriadne si si zamakal! Sila stúpla o 10.")
-        elif typ_vylepsenia == 'obrana':
-            postava.obrana +=10
-            messages.success(request, f"Kováč ti vylepšil brnenie! Obrana stúpla o 10.")
-        postava.save()
-    else:
-        messages.error(request, f"Nemáš dosť zlata!! Chod zberať do lesa.")
-    
-    return redirect('/postavy/')
-
-def registracia(request):
+# --- HAZARD ---
+@login_required
+def hrat_kocky(request, postava_id):
+    postava = get_object_or_404(Postava, id=postava_id, pouzivatel=request.user)
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('zoznam_postav') # Hodí to na hlavnú stranu
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/registracia.html', {'form': form})
+        try: stavka = int(request.POST.get('stavka', 0))
+        except: stavka = 0
+        if 0 < stavka <= postava.zlato:
+            h, k = random.randint(1, 6), random.randint(1, 6)
+            if h > k:
+                postava.zlato += stavka
+                messages.success(request, f"Vyhral si! Ty {h}, krčmár {k}. +{stavka}z")
+            elif h < k:
+                postava.zlato -= stavka
+                messages.error(request, f"Prehral si! Ty {h}, krčmár {k}. -{stavka}z")
+            else: messages.info(request, "Remíza!")
+            postava.save()
+    return redirect('krcma', postava_id=postava.id)
+
+# --- DIVOCINA ---
+@login_required
+def dobrodruzstvo(request, postava_id):
+    postava = get_object_or_404(Postava, id=postava_id, pouzivatel=request.user)
+    zona = request.GET.get('zona', 'razcestie')
+    vsetci = Nepriatel.objects.filter(sila__lt=50) if zona == 'les' else None
+    nepriatel = random.choice(vsetci) if vsetci else None
+    return render(request, 'game/les.html', {'postava': postava, 'nepriatel': nepriatel, 'zona_name': zona})
+
+@login_required
+def bojovat(request, postava_id, nepriatel_id):
+    postava = get_object_or_404(Postava, id=postava_id, pouzivatel=request.user)
+    nepriatel = get_object_or_404(Nepriatel, id=nepriatel_id)
+    dmg = max(0, nepriatel.sila - postava.obrana)
+    postava.hp -= dmg
+    if postava.hp <= 0:
+        postava.delete()
+        return redirect('zoznam_postav')
+    postava.xp += nepriatel.xp_odmena
+    postava.zlato += 20
+    postava.save()
+    messages.warning(request, f"Boj s {nepriatel.nazov}! -{dmg} HP. Si stále v divočine!")
+    return redirect(f"/postava/{postava.id}/boj/?zona=les")
+
+# --- KOVÁČ ---
+@login_required
+def kovac(request, postava_id):
+    postava = get_object_or_404(Postava, id=postava_id, pouzivatel=request.user)
+    inventar = Predmet.objects.filter(majitel=postava)
+    return render(request, 'game/kovac.html', {'postava': postava, 'inventar': inventar})
+
+@login_required
+def nakup_vylepsenia(request, postava_id, typ):
+    postava = get_object_or_404(Postava, id=postava_id, pouzivatel=request.user)
+    if postava.zlato >= 100:
+        postava.zlato -= 100
+        if typ == 'sila': postava.sila += 10
+        else: postava.obrana += 10
+        postava.save()
+    return redirect('kovac', postava_id=postava.id)
--- zakladny_web/settings.py
+++ zakladny_web/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,3 +119,7 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/postavy/'
 LOGIN_URL = '/login/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'game', 'static'),
+]
